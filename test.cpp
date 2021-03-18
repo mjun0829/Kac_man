4c9caead13977fc8c8fe7b3129a03410c4c596f8
jaeseung-lee <kevin0969@naver.com>
2021-03-18 12:28:49 +0900

    DFS

--- test.cpp
+++ test.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
 class test {
 private:
+  int size;
   vector<vector<int>> board;
-  vector<vector<int>> graph;
 
 public:
   test();
   test(int n);
   vector<vector<int>> get_board() const { return board; }
-  vector<vector<int>> get_graph() const { return graph; }
 
   void display(void) const;
+  /*
+  [1][1] to [n-1][n-1]
+  */
+  void find_path(void);
+  void find_path(int now_x, int now_y, vector<vector<int>> footprint,
+                 stack<int> x, stack<int> y);
 };
 
 int main() {
   test a(5);
   a.display();
+  a.find_path();
   return 0;
 }
 
 test::test() {
+  size = 1;
   vector<vector<int>> result;
   vector<int> temp = {0};
   result.push_back(temp);
 }
 
 test::test(int n) {
+  size = n;
   //먼저 보드판 만듦
   vector<vector<int>> result_board;
   for (int i = 0; i < n; i++) {
@@ -48,17 +57,6 @@ test::test(int n) {
   result_board[n - 2][n - 1] = 0;
   result_board[n - 1][n - 2] = 0;
   board = result_board;
-
-  vector<vector<int>> result_graph;
-  for (int i = 0; i < n * n; i++) {
-    vector<int> temp;
-    for (int j = 0; j < n * n; j++) {
-      temp.push_back(0);
-    }
-    result_graph.push_back(temp);
-  }
-
-  graph = result_graph;
 }
 
 void test::display(void) const {
@@ -69,11 +67,40 @@ void test::display(void) const {
     }
     cout << endl;
   }
-  cout << "graph" << endl;
-  for (auto i : graph) {
-    for (auto j : i) {
-      cout << j << " ";
+}
+
+void test::find_path(void) {
+  vector<vector<int>> footprint = board;
+  stack<int> way_x;
+  stack<int> way_y;
+  find_path(1, 1, footprint, way_x, way_y);
+}
+
+void test::find_path(int now_x, int now_y, vector<vector<int>> footprint,
+                     stack<int> way_x, stack<int> way_y) {
+  footprint[now_y][now_x] = 1;
+  way_x.push(now_x);
+  way_y.push(now_y);
+  if (now_x == size - 1 && now_y == size - 1) {
+    cout << "finished : " << way_x.size() << endl;
+    while (way_x.size() != 0) {
+      cout << "x : " << way_x.top() << " y : " << way_y.top() << endl;
+      way_x.pop();
+      way_y.pop();
     }
-    cout << endl;
+    return;
+  }
+
+  if (now_y != 0 && (footprint[now_y - 1][now_x] == 0)) {
+    find_path(now_x, now_y - 1, footprint, way_x, way_y);
+  }
+  if (now_y != size - 1 && (footprint[now_y + 1][now_x] == 0)) {
+    find_path(now_x, now_y + 1, footprint, way_x, way_y);
+  }
+  if (now_x != 0 && (footprint[now_y][now_x - 1] == 0)) {
+    find_path(now_x - 1, now_y, footprint, way_x, way_y);
+  }
+  if (now_x != size - 1 && (footprint[now_y][now_x + 1] == 0)) {
+    find_path(now_x + 1, now_y, footprint, way_x, way_y);
   }
 }
