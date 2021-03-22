b34112217eda26f6282966c5ff430b72410960c3
jaeseung-lee <kevin0969@naver.com>
2021-03-22 15:03:22 +0900

    visualization

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <vector>
 #include <time.h>
+#include <ncurses.h>
 
 using namespace std;
 
@@ -17,6 +18,7 @@ public:
   test(int n);
   vector<vector<int>> get_board() const { return board; }
 
+  void display_init_board(void) const;
   void display(void) const;
   /*
   [1][1] to [n-1][n-1]
@@ -27,13 +29,17 @@ public:
 };
 
 int main() {
-  test a(9);
-  time_t start = clock();
-  a.find_path();
-  time_t end = clock();
-  cout << "time : " << (double)(end - start) / CLOCKS_PER_SEC << " sec " <<endl;
-  a.display();
-  return 0;
+    initscr();
+    test a(50);
+    a.display_init_board();
+    time_t start = clock();
+    a.find_path();
+    time_t end = clock();
+    //a.display();
+    endwin();
+    cout << "time : " << (double)(end - start) / CLOCKS_PER_SEC << " sec "
+         << endl;
+    return 0;
 }
 
 test::test() {
@@ -70,6 +76,19 @@ test::test(int n) {
   }
 }
 
+//목적지가 D, 벽이 @
+void test::display_init_board(void) const {
+  for (int row=0; row < size;row++){
+      for (int col=0; col < size;col++){
+        if(board[row][col]==1){
+            mvprintw(row, col, "@");
+        }
+      }
+  }
+  mvprintw(size - 1, size - 1, "D");
+  refresh();
+}
+
 void test::display(void) const {
   stack<int> temp_x = shortcut_x;
   stack<int> temp_y = shortcut_y;
@@ -90,32 +109,45 @@ void test::find_path(void) {
 
 void test::find_path(int now_x, int now_y, vector<vector<int>> footprint,
                      stack<int> way_x, stack<int> way_y) {
-  if (shortcut_x.size() < way_x.size()){
-      cout << "terminated" << endl;
-      return;
+  //민준이 아이디어
+  if (shortcut_x.size()+shortcut_y.size() < way_x.size()+way_y.size()+(size-now_x)+(size-now_y)) {
+    mvprintw(way_y.top(), way_x.top(), " ");
+    //    cout << "terminated" << endl;
+    refresh();
+    return;
   }
+  mvprintw(now_y, now_x, "O");
+  refresh();
   footprint[now_y][now_x] = 1;
   way_x.push(now_x);
   way_y.push(now_y);
+
   if (now_x == size - 1 && now_y == size - 1) {
-    cout << "finished : " << way_x.size() << endl;
+    //cout << "finished : " << way_x.size() << endl;
+    mvprintw(way_y.top(), way_x.top(), " ");
     if (shortcut_x.size() > way_x.size()) {
       shortcut_x = way_x;
       shortcut_y = way_y;
     }
+      refresh();
     return;
+  }
+
+  if (now_x != size - 1 && (footprint[now_y][now_x + 1] == 0)) {
+    find_path(now_x + 1, now_y, footprint, way_x, way_y);
+  }
+
+  if (now_y != size - 1 && (footprint[now_y + 1][now_x] == 0)) {
+    find_path(now_x, now_y + 1, footprint, way_x, way_y);
+  }
+
+  if (now_x != 0 && (footprint[now_y][now_x - 1] == 0)) {
+    find_path(now_x - 1, now_y, footprint, way_x, way_y);
   }
 
   if (now_y != 0 && (footprint[now_y - 1][now_x] == 0)) {
     find_path(now_x, now_y - 1, footprint, way_x, way_y);
   }
-  if (now_y != size - 1 && (footprint[now_y + 1][now_x] == 0)) {
-    find_path(now_x, now_y + 1, footprint, way_x, way_y);
-  }
-  if (now_x != 0 && (footprint[now_y][now_x - 1] == 0)) {
-    find_path(now_x - 1, now_y, footprint, way_x, way_y);
-  }
-  if (now_x != size - 1 && (footprint[now_y][now_x + 1] == 0)) {
-    find_path(now_x + 1, now_y, footprint, way_x, way_y);
-  }
+  mvprintw(now_y, now_x, " ");
+  refresh();
 }
